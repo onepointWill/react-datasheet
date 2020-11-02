f4072823b22c168bf7bb3edf96a19b9e3f2fe492
William Fussey <william.fussey@onepointltd.com>
2020-11-02 17:46:37 +0000

    Built with react table

--- src/DataSheet.js
+++ src/DataSheet.js
@@ -1,5 +1,5 @@
 import React, { PureComponent } from 'react';
-import ReactTable from 'react-table-6';
+import ReactTable from 'react-table';
 import PropTypes from 'prop-types';
 import Sheet from './Sheet';
 import Row from './Row';
